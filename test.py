0d8b21b4583cd9298076e6c1ed589ec6fe8333f9
thibaud <thibaud.hurelle@hoolders.com>
2018-12-06 10:17:14 +0100

    verions 2.1

--- test.py
+++ test.py
@@ -8,25 +8,51 @@ DATAFILE1 = "test1.csv"
 DATAFILE2 = "test2.csv"
 DATAFILE3 = "test3.csv"
 DATAFILE4 = "test4.csv"
+DATAFILE5 = "test5.csv"
 
 print "---------------DATAFILE1---------------"
 datafile = os.path.join(DATADIR, DATAFILE)
 d = csv2json(datafile)
 pprint.pprint(d)
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
+
+
 print "---------------DATAFILE1---------------"
 datafile = os.path.join(DATADIR, DATAFILE1)
 d = csv2json(datafile)
 pprint.pprint(d)
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
+
+
 print "---------------DATAFILE2---------------"
 datafile = os.path.join(DATADIR, DATAFILE2)
 d = csv2json(datafile)
 pprint.pprint(d)
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
+
+
 print "---------------DATAFILE3---------------"
 datafile = os.path.join(DATADIR, DATAFILE3)
 d = csv2json(datafile)
 pprint.pprint(d)
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
+
+
 print "---------------DATAFILE4---------------"
 datafile = os.path.join(DATADIR, DATAFILE4)
 d = csv2json(datafile)
 pprint.pprint(d)
-print "------------------------------"
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
+
+
+print "---------------DATAFILE5---------------"
+datafile = os.path.join(DATADIR, DATAFILE5)
+d = csv2json(datafile)
+pprint.pprint(d)
+print " -- Input File : %s lines"%(len(open(datafile,"r").readlines()) -1)
+print " -- Out File : %s lines"%len(d)
